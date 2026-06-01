2c041f1f8539bcee45049e1acc6b2991d4988214
Rohan <rk2024@duck.com>
2026-06-02 02:01:13 +0530

    feat : Added 404 responses in GET routes.

--- index.js
+++ index.js
@@ -49,11 +49,43 @@ app.post("/api/addresses", async (req, res) => {
   }
 });
 
+//Function to Update address Data to Database using Mongoose
+
+const updateAddressDataToDatabase = async (addressId, updatedData) => {
+  try {
+    const updateAddressData = await addressModel.findByIdAndUpdate(
+      addressId,
+      updatedData,
+      { new: true }
+    );
+    return updateAddressData;
+  } catch (error) {
+    console.log("Error Occured While Updating Address Data To Database", error);
+  }
+};
+
+//using Above Mongoose function with Express 'POST' Route to Update address Data to Database
+
+app.post("/api/addresses/:addressId", async (req, res) => {
+  try {
+    const addressData = await updateAddressDataToDatabase(
+      req.params.addressId,
+      req.body
+    );
+    res.status(200).json({
+      message: "Address Updated Successfully",
+      data: { address: addressData },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 //function to Delete address Data from Database using Mongoose
 
 const deleteAddressFromDatabase = async (addressId) => {
   try {
-    const deleteAddress = await addressModel.findOneAndDelete(addressId);
+    const deleteAddress = await addressModel.findByIdAndDelete(addressId);
     return deleteAddress;
   } catch (error) {
     console.log(
@@ -93,10 +125,14 @@ const getAddressDataFromDatabase = async () => {
 app.get("/api/fetch/addresses", async (req, res) => {
   try {
     const getAddress = await getAddressDataFromDatabase();
-    res.status(200).json({
-      message: "Fetched All Addresses From Database Successfully.",
-      data: { address: getAddress },
-    });
+    if (!getAddress) {
+      res.status(404).json({ message: "Address Data Not Found." });
+    } else {
+      res.status(200).json({
+        message: "Fetched All Addresses From Database Successfully.",
+        data: { address: getAddress },
+      });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -150,10 +186,14 @@ const getClothingDataFromTheDatabase = async () => {
 app.get("/api/fetch/Clothing", async (req, res) => {
   try {
     const clothingData = await getClothingDataFromTheDatabase();
-    res.status(200).json({
-      message: "Fetched All Clothing Data From Database Successfully.",
-      data: { clothing: clothingData },
-    });
+    if (!clothingData) {
+      res.status(404).json({ message: "Clothing Data Not Found." });
+    } else {
+      res.status(200).json({
+        message: "Fetched All Clothing Data From Database Successfully.",
+        data: { clothing: clothingData },
+      });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -178,10 +218,16 @@ const getClothingDataUsingId = async (clothingId) => {
 app.get("/api/Clothing/:clothingId", async (req, res) => {
   try {
     const clothingData = await getClothingDataUsingId(req.params.clothingId);
-    res.status(200).json({
-      message: "Fetched All Clothing Data Using Id From the Database.",
-      data: { clothing: clothingData },
-    });
+    if (!clothingData) {
+      res
+        .status(404)
+        .json({ message: "This Specific Clothing Data Not Found." });
+    } else {
+      res.status(200).json({
+        message: "Fetched All Clothing Data Using Id From the Database.",
+        data: { clothing: clothingData },
+      });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -235,10 +281,14 @@ const getCategoriesDataFromtheDatabase = async () => {
 app.get("/api/fetch/categories", async (req, res) => {
   try {
     const categoryData = await getCategoriesDataFromtheDatabase();
-    res.status(200).json({
-      message: "Fetched All Category from the Database.",
-      data: { category: categoryData },
-    });
+    if (!categoryData) {
+      res.status(404).json({ message: "Category Data Not Found." });
+    } else {
+      res.status(200).json({
+        message: "Fetched All Category from the Database.",
+        data: { category: categoryData },
+      });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -293,13 +343,18 @@ const getWishlistDatafromDatabase = async () => {
 
 //using Above Mongoose function with Express 'GET' Route to Fetch wishlist Data from the Database
 
-app.get("/api/fetch/wishlist", async (req,res) => {
+app.get("/api/fetch/wishlist", async (req, res) => {
   try {
     const wishlistData = await getWishlistDatafromDatabase();
-    res.status(200).json({
+     if (!wishlistData) {
+      res.status(404).json({message:"Wishlist Data not Found."});
+    } else {
+      res.status(200).json({
       message: "wishlist Data Successfully Fetched from the Database.",
       data: { wishList: wishlistData },
     });
+    }
+    
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -341,19 +396,30 @@ const getCartDataFromTheDatabase = async () => {
     const getCartData = await userModel.find().select("cart");
     return getCartData;
   } catch (error) {
-    console.log("Error Occured While fetching Cart Data from the Database using Mongoose", error);
+    console.log(
+      "Error Occured While fetching Cart Data from the Database using Mongoose",
+      error
+    );
   }
 };
 
 //using Above Mongoose function with Express 'GET' Route to Fetch Cart Data from the Database
 
-app.get("/api/fetch/cart", async (req,res)=>{
-try {
+app.get("/api/fetch/cart", async (req, res) => {
+  try {
     const cartData = await getCartDataFromTheDatabase();
-    res.status(200).json({message:"Cart Data Successfully Fetched from the Database.", data: { cart: cartData } });
-} catch (error) {
-res.status(500).json({error:error.message});    
-}
+     if (!cartData) {
+      res.status(404).json({message:"Cart Data Not Found."})
+    } else {
+      
+    }
+    res.status(200).json({
+      message: "Cart Data Successfully Fetched from the Database.",
+      data: { cart: cartData },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 //function to Update Order Section in Database using Mongoose
@@ -400,24 +466,35 @@ app.post("/api/orders", async (req, res) => {
 
 //function to Fetch Order Data from the Database using Mongoose
 
-const getOrderDataFromDatabase = async ()=>{
-try {
-    const getOrderData = await userModel.find().select('orders');
+const getOrderDataFromDatabase = async () => {
+  try {
+    const getOrderData = await userModel.find().select("orders");
     return getOrderData;
-} catch (error) {
-    console.log("Error Occured While fetching Order Data from the Database using Mongoose",error);
-}
-}
+  } catch (error) {
+    console.log(
+      "Error Occured While fetching Order Data from the Database using Mongoose",
+      error
+    );
+  }
+};
 
 //using Above Mongoose function with Express 'GET' Route to Fetch Orders Data from the Database
 
-app.get("/api/fetch/orders", async (req,res)=>{
-try {
-   const orderData = await getOrderDataFromDatabase();
-   res.status(200).json({message:"Order Data Successfully Fetched from the Database.", data: { cart: orderData } });
-} catch (error) {
- res.status(500).json({error:error.message});   
-}
+app.get("/api/fetch/orders", async (req, res) => {
+  try {
+    const orderData = await getOrderDataFromDatabase();
+     if (!orderData) {
+      res.status(404).json({message:"Order Data Not Found."})
+    } else {
+    res.status(200).json({
+      message: "Order Data Successfully Fetched from the Database.",
+      data: { cart: orderData },
+    });  
+    }
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const PORT = 3000;
